4cb503e6adc5e2b3d67914667b96b1d42a611a63
apogano <apogano@gmail.com>
2026-07-20 23:34:00 +0300

    Initialize Spring boot project

---
+++ src/main/java/com/apogano/foldersync/FolderSyncServiceApplication.java
@@ -0,0 +1,13 @@
+package com.apogano.foldersync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolderSyncServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolderSyncServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apogano/foldersync/FolderSyncServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apogano.foldersync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolderSyncServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
